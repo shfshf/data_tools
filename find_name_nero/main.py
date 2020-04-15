0fe0dcbc1aac626042d66d8aa4e17a43a9d79458
SongHanFeng <hanfeng.song@ecarx.com.cn>
2020-04-15 10:09:56 +0800

    model name add

---
+++ find_name_nero/main.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+
+with tf.Session() as sess:
+    sm = tf.saved_model.load(
+        sess, [tf.saved_model.tag_constants.SERVING], "./deliverable_model/asset/model/tensorflow_saved_model/1586914508"
+    )
+
+    graph = tf.get_default_graph()
+    tvc = graph.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES)
+    print(tvc)
